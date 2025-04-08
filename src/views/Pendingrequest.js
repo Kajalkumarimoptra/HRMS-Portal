6088a422d705d6435c56df255918781b4060d021
Kajalkumarimoptra <kajal.kumari@moptra.com>
2025-04-08 15:51:05 +0530

    grid design

--- src/views/Pendingrequest.js
+++ src/views/Pendingrequest.js
@@ -10,25 +10,28 @@ export default function Pendingrequest() {
 
     const pendingrequestlist = [
         {
-            id: 1, heading: 'Assets (2)', asset: [
+            id: 1, heading: 'Assets (2)', icon: require("assets/img/assets-list-icon.png"),
+            asset: [
                 { name: "Amit Raj", assetname: "Laptop" },
                 { name: "Sonam Kumari", assetname: "Headphones" }
             ]
         },
-        { id: 2, heading: 'Attendance Corrections (1)' },
+        { id: 2, heading: 'Attendance Corrections (1)', icon: require("assets/img/attendance-correction-icon.png") },
         {
-            id: 3, heading: 'Documents (2)', docscontent: [
+            id: 3, heading: 'Documents (2)', icon: require("assets/img/docs-list-icon.png"),
+            docscontent: [
                 { name: "Amit Raj", doc: "Final Year Marksheet" },
                 { name: "Sonam Kumari", doc: "Intermediate Passing Certificate" }
             ]
         },
         {
-            id: 4, heading: 'Leaves (3)', details: [
+            id: 4, heading: 'Leaves (3)', icon: require("assets/img/leaveslist-icon.png"),
+            details: [
                 { name: "Amit Raj", date: "20-02-2025" },
                 { name: "Sonam Kumari", date: "22-02-2025" }
             ]
         },
-        { id: 5, heading: 'Resignations (2)' }
+        { id: 5, heading: 'Resignations (2)', icon: require("assets/img/resignation-list-icon.png") }
     ];
 
     const redirectUrls = {
@@ -50,13 +53,16 @@ export default function Pendingrequest() {
                 <h5 style={{ fontWeight: 'bold' }}>All Requests</h5>
             </div>
             <div className='pendingrequest-upper-container'>
-                <div class="row flex-column pendingrequest-container">
+                <div class="pendingrequest-container">
                     {pendingrequestlist.map((list) => (
                         <div class="col-md-12 mb-3" key={list.id}>
                             <div>
                                 <div class="pending-request-card">
                                     <div class="header-container" onClick={() => navigate(redirectUrls[list.id])} style={{ cursor: 'pointer' }}>
-                                        <p><b>{list.heading}</b></p>
+                                        <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+                                            <img src={list.icon} alt="icon" style={{ width: '25px', height: '25px', marginTop: '-13px' }} />
+                                            <p><b>{list.heading}</b></p>
+                                        </div>
                                         <div className='clearance-btn'>
                                             <img
                                                 src={require("assets/img/close-dropdown-icon.png")}
@@ -64,9 +70,9 @@ export default function Pendingrequest() {
                                                 onClick={(e) => {
                                                     e.stopPropagation();
                                                     navigate(redirectUrls[list.id]);
-                                                  }}
+                                                }}
                                                 className="icon-redirect" />
-                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
